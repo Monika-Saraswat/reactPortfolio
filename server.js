376267cddeba46c6964b33ec028341bde4e8b6e6
Monika-Saraswat <saraswatmonika55@gmail.com>
2024-10-26 11:25:32 +0530

    link

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const app=express()
 app.use(cors({
     origin: 'https://portfoliomonika.onrender.com',
     methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type']
+    allowedHeaders: ['Content-Type'],
+    credentials: true
   }));
 app.use(express.json())
 app.use('/',router)
